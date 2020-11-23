d1b5eebfdbf7c639792facce68ba446fc1e90ddf
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-11-23 11:20:31 +0100

    tp_3-2 gestion de produits

---
+++ tp_3-2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+nbreProduit = input("Saisir le nombre de produits à gérer ")
+nbP = int(nbreProduit)
+
+for i in range (0, nbP):
+
+  quantity = input("Saisir une quantité ")
+  q = int(quantity)
+  price = input("Saisir un prix ")
+  p = float(price)
+
+  if(q <= 0 or p <= 0):
+    print("Veuillez saisir des données correctes")
+    break
+
+  else:
+    totalPrice = round(q * (p * 1.2), 2)
+
+    if (totalPrice > 200):
+      remisePrice = float(totalPrice * 0.05)
+      newPrice = round(totalPrice - remisePrice, 2)
+      print(newPrice)
+  
+    else :
+      print(totalPrice)
