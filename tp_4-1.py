87b3142da96f2d70b753f9a8b5919e6b9c962056
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-11-23 20:24:13 +0100

    Add total HT for one article

--- tp_4-1.py
+++ tp_4-1.py
@@ -21,24 +21,37 @@ for (key,value) in dProduct.items():
 print(table)
 
 
-nbreProduit = input("Saisir le nombre de produits à acheter ")
+nbreProduit = input("Saisir le nombre de produits à acheter : ")
 nbP = int(nbreProduit)
 
 for i in range (0, nbP):
 
-  productChosen = input("Saisir l'Id du produit à ajouter au panier ")
+  productChosen = input("Saisir l'id du produit à ajouter au panier : ")
   pdtCh = int(productChosen)
 
-  print(table[pdtCh - 1 ])
+  try:
+    print(table[pdtCh - 1 ]) #table index start with 0
 
-  quantity = input("Saisir une quantité ")
-  q = int(quantity)
+    quantity = input("Saisir une quantité : ")
+    q = int(quantity)
 
-  priceProductChosen = dProduct[pdtCh].get('price')
-  prPdtCh = float(priceProductChosen)
+    priceProductChosen = dProduct[pdtCh].get('price')
+    prPdtCh = float(priceProductChosen)
 
-  totalHT = round((q*prPdtCh),2)
-  print(totalHT +'€')
+    totalHT = round((abs(q)*prPdtCh), 2)
+    totalHT = str(totalHT)
+    print('Total HT : ' + totalHT +'€')
+
+
+  except IndexError: 
+    print("Veuillez saisir un id valide. ")
+    break
+
+
+  except ValueError: 
+    print("Veuillez saisir une quantité valide. ")
+    break
+
 
 
 
