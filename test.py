89837a9e3ad65deb990cecb46b19791fec947ef9
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-11-23 10:38:39 +0100

    tp_3-1 calcul

---
+++ test.py
@@ -0,0 +1,3 @@
+for i in range (5,8):
+  print(i, i ** 2)
+print('Fin de boucle')
---
+++ tp_3-1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+quantity = input("Saisir une quantité ")
+q = float(quantity)
+price = input("Saisir un prix ")
+p = float(price)
+
+totalPrice = float(q * (p * 1.2))
+print(totalPrice)
+
+
