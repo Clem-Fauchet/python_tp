91745ec74b1efe5088636b654bf85f98364ab22a
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-11-24 10:57:44 +0100

    test module input variable

---
+++ tp_4-2/input.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+def userInput(string, variable):
+
+  inputUser = input("Saisir" + string +  ": ")
+  variable = int(inputUser)
+  return variable
+
+
+
+
---
+++ tp_4-2/main.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import calcul
+import input
+import input 
+
+from prettytable import PrettyTable
+
+
+dProduct = {
+            1: {'name':'Banane', 'price': 4}, 
+            2: {'name':'Pomme', 'price': 2}, 
+            3: {'name':'Orange', 'price': 1.5},
+            4: {'name':'Poire', 'price': 3}
+          }
+
+
+table = PrettyTable(['Id', 'Name', 'Price']) # first table dictionnary
+
+for (key,value) in dProduct.items():
+   table.add_row([key, value['name'], value['price']])
+
+
+print(table)
+
+totalPaid = 0.0 #init total
+
+print(input.userInput("le nombre de produits à acheter", nbP))
+
+
+
+for i in range (0, nbP): #loop pour each product
+
+  print(input.userInput("l'id du produit à ajouter au panier", pdtCh))
+
+  try:
+    print(table[pdtCh - 1 ]) #table index start with 0
+
+    print(input.userInput("une quantité", q))
+
+
+
+  except IndexError: #si id out of range
+    print("Veuillez saisir un id valide. ")
+    break
+
+
+  except ValueError: #si quantité float number
+    print("Veuillez saisir une quantité valide. ")
+    break
