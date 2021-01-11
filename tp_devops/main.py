72239739fdaf92001ef738a991bc04c698bd8bdd
Clem-Fauchet <cfauchet.pro@gmail.com>
2021-01-11 11:04:33 +0100

    print region

--- tp_devops/main.py
+++ tp_devops/main.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python
 import csv
+import os
+with open('departements-france.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
