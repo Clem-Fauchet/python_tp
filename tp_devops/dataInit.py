5d47308824c377d6b556bfa3281415073007cd6f
Clem-Fauchet <cfauchet.pro@gmail.com>
2021-01-12 14:50:22 +0100

    clean

--- tp_devops/dataInit.py
+++ tp_devops/dataInit.py
@@ -4,12 +4,6 @@ import os
 import pandas as pd
 
 
-# with open("tp_devops/data/departements-france.csv", newline="") as csvfile:
-#     spamreader = csv.reader(csvfile, delimiter=" ", quotechar="|")
-#     for row in spamreader:
-#         print(", ".join(row))
-
-
 with open("tp_devops/data/departements-france.csv", "r", encoding="utf8") as f:
     # df = pd.read_csv("tp_devops/data/departements-france.csv")
     os.mkdir("tp_devops/cible")
--- tp_devops/main.py
+++ tp_devops/main.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
-import csv
-import os
-
 from csv import DictReader
 from typing import List, Dict
-
-# with open("departements-france.csv", newline="") as csvfile:
-#     spamreader = csv.reader(csvfile, delimiter=" ", quotechar="|")
-#     for row in spamreader:
-#         print(", ".join(row))
 
 file_handle = open("tp_devops/data/departements-france.csv", "r", encoding="utf8")
 csv_reader = DictReader(file_handle)
